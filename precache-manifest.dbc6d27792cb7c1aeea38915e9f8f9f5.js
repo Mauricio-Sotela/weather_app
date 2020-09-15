a6bc3e57f2a4eb3e1571f145ffcf1020064f2da1
sotela88 <j-ms-27@hotmail.com>
2020-09-15 19:19:08 +0200

    Updates

---
+++ precache-manifest.dbc6d27792cb7c1aeea38915e9f8f9f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6fb233ff763cafdacc132e2ccf47571",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "6c271658d768d80fb4f0",
+    "url": "/weather_app/static/css/main.0348f9c9.chunk.css"
+  },
+  {
+    "revision": "d6d7f067d588f1f7d9c9",
+    "url": "/weather_app/static/js/2.50129419.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather_app/static/js/2.50129419.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c271658d768d80fb4f0",
+    "url": "/weather_app/static/js/main.ac8b221f.chunk.js"
+  },
+  {
+    "revision": "a1e933fa564276263c3d",
+    "url": "/weather_app/static/js/runtime-main.633fb810.js"
+  },
+  {
+    "revision": "17e23c83cac491ae95b630c3f8232957",
+    "url": "/weather_app/static/media/logo.17e23c83.png"
+  }
+]);
