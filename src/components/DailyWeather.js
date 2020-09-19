b8da0352a5163d2c1fa5962e3aa23b08910d0b5d
sotela88 <j-ms-27@hotmail.com>
2020-09-19 12:13:12 +0200

    daily temp

--- src/components/DailyWeather.js
+++ src/components/DailyWeather.js
@@ -7,7 +7,7 @@ function DailyWeather(props) {
   return (
     <div className="daily">
       <div className="day">{new Date(props.data.dt * 1000).toString().substring(0, 10)}</div>
-      <div className="temp">{Math.round(props.data.temp.day)}°</div>
+      <div className="temp">{Math.round(props.data.temp.min)}° / {Math.round(props.data.temp.max)}°</div>
       <div className="icon">
           <img src={`http://openweathermap.org/img/wn/${props.data.weather[0].icon}@2x.png`} alt='icon'/>
       </div>
